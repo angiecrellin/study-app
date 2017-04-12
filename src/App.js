1902f327b4586f47a8db124338c1b5557ff4ee85
angiecrellin <angiecrellin@gmail.com>
2017-04-12 09:39:09 -0500

    style updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
-// import SearchBar from './containers/SearchBar'
+
 import Loader from './containers/Loader'
-// import FlashcardSet from './containers/FlashcardSet'
+
 import FlashcardDisplay from './containers/FlashcardDisplay'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*SearchBar*/}
         <Loader />
-        {/*FlashcardSet /> */}
         <FlashcardDisplay />
 
       </div>
